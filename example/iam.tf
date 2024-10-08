28ad56bbe1be0ce381f2001c00d1c6ac02c192d1
Damien LEFLON <damien.leflon@gmail.com>
2024-10-08 21:19:16 +0200

    chore: example consistency

--- example/iam.tf
+++ example/iam.tf
@@ -2,6 +2,6 @@
 module "repo_gh_oidc_setup" {
   source = "github.com/dmlf/tf-iam-gha-config?ref=0.1.0"
   
-  permission_config_files = fileset("${path.module}/config", "GH_OIDC_*")
+  permission_config_files = fileset("${path.module}/config", "gha_permissions_*")
   
 }
